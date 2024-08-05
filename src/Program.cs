2680fe3de10fa0ae3723a73066f878efb3202c57
zacw-243L <qwerty.amousy123442x@gmail.com>
2024-08-05 13:15:10 +0800

    pass 1st stage

--- src/Program.cs
+++ src/Program.cs
@@ -1,37 +1,42 @@
 using System;
 using System.IO;
 
-static bool MatchPattern(string inputLine, string pattern)
+class Program
 {
-    if (pattern.Length == 1)
+    static bool MatchPattern(string inputLine, string pattern)
     {
-        return inputLine.Contains(pattern);
+        if (pattern.Length == 1)
+        {
+            return inputLine.Contains(pattern);
+        }
+        else
+        {
+            throw new ArgumentException($"Unhandled pattern: {pattern}");
+        }
     }
-    else
+
+    static void Main(string[] args)
     {
-        throw new ArgumentException($"Unhandled pattern: {pattern}");
+        if (args.Length < 2 || args[0] != "-E")
+        {
+            Console.WriteLine("Expected first argument to be '-E'");
+            Environment.Exit(2);
+        }
+
+        string pattern = args[1];
+        string inputLine = Console.In.ReadToEnd();
+
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        Console.WriteLine("Logs from your program will appear here!");
+
+        // Uncomment this block to pass the first stage
+        if (MatchPattern(inputLine, pattern))
+        {
+            Environment.Exit(0);
+        }
+        else
+        {
+            Environment.Exit(1);
+        }
     }
-}
-
-if (args[0] != "-E")
-{
-    Console.WriteLine("Expected first argument to be '-E'");
-    Environment.Exit(2);
-}
-
-string pattern = args[1];
-string inputLine = Console.In.ReadToEnd();
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
-
-Uncomment this block to pass the first stage
- 
- if (MatchPattern(inputLine, pattern))
- {
-    Environment.Exit(0);
-}
- else
- {
-    Environment.Exit(1);
 }
