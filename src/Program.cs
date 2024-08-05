ad5b37c1d55477e31cb26ada88f3a0df3322e41d
zacw-243L <qwerty.amousy123442x@gmail.com>
2024-08-05 13:31:59 +0800

    Pass Stage 5

--- src/Program.cs
+++ src/Program.cs
@@ -2,31 +2,31 @@ using System;
 using System.IO;
 using System.Text.RegularExpressions;
 
-static bool MatchPattern(string inputLine, string pattern)
-{
-    if (@"\d\w".Contains(pattern))
-    {
+static bool MatchPattern(string inputLine, string pattern) {
+    if (@"\d\w".Contains(pattern)) {
         return Regex.IsMatch(inputLine, pattern);
     }
-    else if (pattern[0] == '[' && pattern[^1] == ']')
-    {
-        for (int i = 1; i < pattern.Length - 1; i++)
-            if (inputLine.Contains(pattern[i]))
-                return true;
-        return false;
-    }
-    else if (pattern.Length == 1)
-    {
+    else if (pattern.StartsWith('[') && pattern.EndsWith(']')) {
+        var strippedPattern = pattern.Trim(['[', ']', '^']);
+        if (pattern.Contains('^')) {
+            foreach (var c in strippedPattern)
+                if (inputLine.Contains(c))
+                    return false;
+            return true;
+        } else {
+            foreach (var c in strippedPattern)
+                if (inputLine.Contains(c))
+                    return true;
+            return false;
+        }
+    } else if (pattern.Length == 1) {
         return inputLine.Contains(pattern);
-    }
-    else
-    {
+    } else {
         throw new ArgumentException($"Unhandled pattern: {pattern}");
     }
 }
 
-if (args.Length < 2 || args[0] != "-E")
-{
+if (args[0] != "-E") {
     Console.WriteLine("Expected first argument to be '-E'");
     Environment.Exit(2);
 }
@@ -37,11 +37,8 @@ string inputLine = Console.In.ReadToEnd();
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-if (MatchPattern(inputLine, pattern))
-{
+if (MatchPattern(inputLine, pattern)) {
     Environment.Exit(0);
-}
-else
-{
+} else {
     Environment.Exit(1);
 }
