6bc9623084ed24041fcebf02403463897c563088
zacw-243L <qwerty.amousy123442x@gmail.com>
2024-08-05 13:24:29 +0800

    pass stage 4

--- src/Program.cs
+++ src/Program.cs
@@ -2,46 +2,46 @@ using System;
 using System.IO;
 using System.Text.RegularExpressions;
 
-class Program
+static bool MatchPattern(string inputLine, string pattern)
 {
-    static bool MatchPattern(string inputLine, string pattern)
+    if (@"\d\w".Contains(pattern))
     {
-        if (pattern.Contains(@"\d") || pattern.Contains(@"\w"))
-        {
-            return Regex.IsMatch(inputLine, pattern);
-        }
-        else if (pattern.Length == 1)
-        {
-            return inputLine.Contains(pattern);
-        }
-        else
-        {
-            throw new ArgumentException($"Unhandled pattern: {pattern}");
-        }
+        return Regex.IsMatch(inputLine, pattern);
     }
-
-    static void Main(string[] args)
+    else if (pattern[0] == '[' && pattern[^1] == ']')
     {
-        if (args.Length < 2 || args[0] != "-E")
-        {
-            Console.WriteLine("Expected first argument to be '-E'");
-            Environment.Exit(2);
-        }
-
-        string pattern = args[1];
-        string inputLine = Console.In.ReadToEnd();
-
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        Console.WriteLine("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-        if (MatchPattern(inputLine, pattern))
-        {
-            Environment.Exit(0);
-        }
-        else
-        {
-            Environment.Exit(1);
-        }
+        for (int i = 1; i < pattern.Length - 1; i++)
+            if (inputLine.Contains(pattern[i]))
+                return true;
+        return false;
     }
+    else if (pattern.Length == 1)
+    {
+        return inputLine.Contains(pattern);
+    }
+    else
+    {
+        throw new ArgumentException($"Unhandled pattern: {pattern}");
+    }
+}
+
+if (args.Length < 2 || args[0] != "-E")
+{
+    Console.WriteLine("Expected first argument to be '-E'");
+    Environment.Exit(2);
+}
+
+string pattern = args[1];
+string inputLine = Console.In.ReadToEnd();
+
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+Console.WriteLine("Logs from your program will appear here!");
+
+if (MatchPattern(inputLine, pattern))
+{
+    Environment.Exit(0);
+}
+else
+{
+    Environment.Exit(1);
 }
